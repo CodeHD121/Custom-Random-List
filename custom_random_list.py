230890b856a423d37d8bb313f240eaa27c08bc89
wazdalos <91540358+wazdalos@users.noreply.github.com>
2022-10-28 10:59:33 +0200

    Add files via upload

---
+++ custom_random_list.py
@@ -0,0 +1,9 @@
+# creates a random list, with x amounts of digits (nums_limit), between y(start) and z(finish)
+def random_list(nums_limit,start,finish):
+    import random
+    nums_list = []
+    nums_count = 0
+    while nums_count <= nums_limit:
+        nums_list.append(random.randint(start,finish))
+        nums_count += 1
+    return nums_list
